8feaa4f8f764277bbd9daf82b986502cd4a80181
Adrian Alvarado <adrian_1981@hotmail.com>
2022-03-12 13:24:17 -0600

    2do commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+//import styles from "./styles.css"
 
-function App() {
+const App = () => {
+  const [ValorTemperatura, SetValorTemperatura] = useState(10);
+  const [ColorTemperatura, SetColorTemperatura] = useState("cold");
+
+  // const edad = 40;
+  // const mensaje = `mi edad es: ${edad}`;
+  // alert(mensaje);
+
+  const IncrementarTemperatura = () => {
+    if (ValorTemperatura === 30) return;
+
+    const NuevaTemperatura = ValorTemperatura + 1
+
+    if(NuevaTemperatura >= 18) {
+      SetColorTemperatura("neutral");
+
+      if(NuevaTemperatura >= 24) {
+        SetColorTemperatura("hot");
+      }
+    }
+    
+    SetValorTemperatura(NuevaTemperatura);
+  }
+
+  const DecrementarTemperatura = () => {
+    if (ValorTemperatura === 0) return;
+
+    const NuevaTemperatura = ValorTemperatura - 1
+
+    if(NuevaTemperatura <= 18) {
+      SetColorTemperatura("neutral");
+
+      if(NuevaTemperatura <= 12) {
+        SetColorTemperatura("cold");
+      }
+    }
+
+    SetValorTemperatura(NuevaTemperatura);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      
+      <div className="div.temperature-display-container">        
+        {/* <div className="temperature-display hot">{ValorTemperatura}°C</div> */}
+        <div className={`temperature-display ${ColorTemperatura}`}>{ValorTemperatura}°C</div>
+      </div>
+
+      <div className="button-container">
+        {/* <button onClick={() => SetValorTemperatura(ValorTemperatura+1)} >+</button>
+        <button onClick={() => SetValorTemperatura(ValorTemperatura-1)} >-</button> */}
+
+        <button onClick={() => IncrementarTemperatura()} >+</button>
+        <button onClick={() => DecrementarTemperatura()} >-</button>
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
